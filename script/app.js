48175518abbbd1adaf241cf77a410a74407d1292
FLOWOFRIVER <77009997+FLOWOFRIVER@users.noreply.github.com>
2023-08-15 15:35:34 +0100

    Add files via upload

---
+++ script/app.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', function(event) {
+    // event.preventDefault(); 
+
+    const pullups = document.querySelector('#pullups');
+    console.log(pullups.value); // Assuming you want to log the form element
+
+    
+});
